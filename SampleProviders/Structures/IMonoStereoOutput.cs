7f4f65c136bca1ac921ade376ff6cc5576fb734b
NycroV <83246959+NycroV@users.noreply.github.com>
2024-09-17 18:34:46 -0400

    Add update method for MSOutputs

---
+++ SampleProviders/Structures/IMonoStereoOutput.cs
@@ -0,0 +1,17 @@
+﻿using MonoStereo.SampleProviders;
+
+namespace MonoStereo
+{
+    public interface IMonoStereoOutput
+    {
+        public int DesiredLatency { get; }
+
+        void Init(AudioMixer waveProvider);
+
+        void Play();
+
+        void Update();
+
+        void Dispose();
+    }
+}
