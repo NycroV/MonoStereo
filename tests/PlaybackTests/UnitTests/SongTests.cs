8e8b7cb68ee7ec26ae387f55749b139a26b33fe5
Nycro <hajeknolan@gmail.com>
2024-10-21 23:01:13 -0400

    Test updates

--- tests/PlaybackTests/UnitTests/SongTests.cs
+++ tests/PlaybackTests/UnitTests/SongTests.cs
@@ -1,23 +1,52 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using MonoStereo.Sources.Songs;
+using System.Windows.Input;
+
 namespace PlaybackTests.UnitTests
 {
     [TestClass]
     public class SongTests
     {
-        static bool ShutdownEngine;
+        static readonly QueuedLock TestLock = new();
 
         const string Assets = @"../../../../TestAssets";
+
+        const string CompiledAssets = $"{Assets}/Compiled";
+
+        const string RawAssets = $"{Assets}/Raw";
+
+        static bool ShutdownEngine = false;
 
         [TestMethod]
         public void PlaySong()
         {
             ShutdownEngine = false;
+
             AudioManager.Initialize(() => ShutdownEngine, 1f, 1f, 1f);
 
-            Song song = Song.Create($"{Assets}/Compiled/Navigating");
+            Song song = Song.Create($"{CompiledAssets}/Navigating");
             song.Play();
 
-            // Just play the song for 5 seconds
-            Thread.Sleep(TimeSpan.FromSeconds(5));
+            float seconds = 5; // (float)(song.Source as SongReader).Length / AudioStandards.SampleRate / AudioStandards.ChannelCount;
+            Logger.LogMessage("Sleeping for {0} seconds", seconds);
+
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            ShutdownEngine = true;
+        }
+
+        [TestMethod]
+        public void PlayBufferedSong()
+        {
+            ShutdownEngine = false;
+            AudioManager.Initialize(() => ShutdownEngine, 1f, 1f, 1f);
+
+            Song song = Song.CreateBuffered($"{CompiledAssets}/Navigating");
+            song.Play();
+
+            float seconds = 5; // (float)(song.Source as SeekableBufferedSongReader).Length / AudioStandards.SampleRate / AudioStandards.ChannelCount;
+            Logger.LogMessage("Sleeping for {0} seconds", seconds);
+
+            Thread.Sleep(TimeSpan.FromSeconds(seconds));
             ShutdownEngine = true;
         }
     }
